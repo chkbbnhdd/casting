e64309baaff8d76d93b26c9b56226b9b83f409cd
Chakib Grube Benhaddou <drexchabe@dr.dk>
2026-06-09 12:29:31 +0200

    chnge config v11

--- src/app/receiver-page/receiver-page.component.ts
+++ src/app/receiver-page/receiver-page.component.ts
@@ -12,7 +12,7 @@ declare global {
 const CAST_RECEIVER_SCRIPT_URL = 'https://www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js';
 const NEXT_UP_PREVIEW_SECONDS = 30;
 const CONTROLS_HIDE_DELAY_MS = 5000;
-const SHOW_DEBUG_OVERLAY = true;
+const SHOW_DEBUG_OVERLAY = false;
 const CONFIG_ENDPOINT_URL = 'https://prod95-cdn.dr-massive.com/api/config?device=chromecast&ff=idp%2Cldp%2Crpt&include=classification%2Csubscription%2Csitemap%2Cnavigation%2Cgeneral%2Ci18n%2Cplayback%2Clinear%2CfeatureFlags&lang=da&segments=drtv&sub=Registered';
 const PAGE_ENDPOINT_BASE_URL = 'https://prod95-cdn.dr-massive.com/api/page';
 const VIDEO_ENDPOINT_BASE_URL = 'https://prod95.dr-massive.com/api/account/items';
