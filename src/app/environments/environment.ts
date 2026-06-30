0442e3b3ca61883220ec0987ea6e883666bbe24e
Chakib Grube Benhaddou <drexchabe@dr.dk>
2026-06-30 08:19:01 +0200

    update v18

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   receiver: {
     castReceiverScriptUrl: 'https://www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js',
     debugEventThrottleMs: 500,
-    showDebugOverlay: true,
+    showDebugOverlay: false,
     configEndpointUrl: 'https://prod95-cdn.dr-massive.com/api/config?device=chromecast&ff=idp%2Cldp%2Crpt&include=classification%2Csubscription%2Csitemap%2Cnavigation%2Cgeneral%2Ci18n%2Cplayback%2Clinear%2CfeatureFlags&lang=da&segments=drtv&sub=Registered',
     pageEndpointBaseUrl: 'https://prod95-cdn.dr-massive.com/api/page',
     videoEndpointBaseUrl: 'https://prod95.dr-massive.com/api/account/items',
